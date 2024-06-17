0b527e120ab778ac72f7db284cb88c394a4b2c69
Andrew Miracle <me@andrewmiracle.com>
2024-06-17 21:46:47 +0000

    checkpoint 1.1

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#! bin/sh
+
+# cleanup node modules to avoid conflicts
+npx lerna clean
---
+++ yalc.sh
@@ -0,0 +1,6 @@
+#! bin/sh
+
+# install yalc for local package resolution
+# yalc will need to be installed globally
+# @see https://www.npmjs.com/package/yalc
+npm i yalc -g
