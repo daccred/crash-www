c6838af292a98541ed15f6e573399bb51b8bbb5f
Andrew Miracle <me@andrewmiracle.com>
2024-06-20 03:20:44 +0000

    update yalc linking mechanism

--- yalc.sh
+++ yalc.sh
@@ -1,6 +1,28 @@
-#! bin/sh
+#!/bin/bash
 
-# install yalc for local package resolution
-# yalc will need to be installed globally
-# @see https://www.npmjs.com/package/yalc
-npm i yalc -g
+# Ensure the script exits on any error
+set -e
+
+# Define the submodule directories
+submodules=("./runtime" "./blocks" "./gui")
+
+# Add each submodule
+for submodule in "${submodules[@]}"; do
+    echo "updating submodule $submodule..."
+    git submodule init
+    git submodule update
+done
+
+# Navigate into each submodule directory, publish with yalc, and add to the project
+for submodule in "${submodules[@]}"; do
+    echo "Publishing $submodule..."
+    (cd $submodule && yalc publish)
+
+    # Extract the base name and prepend 'scalez-' to construct the package name
+    package_name="scalez-$(basename $submodule)"
+
+    echo "Adding $package_name to the project..."
+    yalc add $package_name
+done
+
+echo "Submodules added and published successfully."
